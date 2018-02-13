7f5e122b226a66092ba762a0b8e2108437491318
santiago vassoler <santiagovassoler@192.168.1.101>
2018-02-13 23:20:29 +0000

    organizing...

--- lib/emperor/handler.ex
+++ lib/emperor/handler.ex
@@ -1,46 +1,132 @@
 defmodule Emperor.Handler do
+import Emperor.FileHandler, only: [handle_file: 2]
+import Emperor.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+import Emperor.Parser, only: [parse: 1]
+
+@pages_path Path.expand("pages", File.cwd!)
+
   def handle(request) do
-  #  conv = parse(request)
-  #  conv = route(conv)
-  #  format_response(conv)
-  request
-  |> parse
-  |> route
-  |> format_response
+    request
+    |> parse
+    |> rewrite_path
+    |> log
+    |> route
+    |> track
+    |> format_response
   end
 
-  def parse(request) do
-    #first_line = request |> String.split("\n") |> List.first
-    #[method, path , _] = String.split(first_line, " ")
-    # %{ method: method, path: path, resp_body: "" }
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" }
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
-  def route(conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers"}
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+    @pages_path
+    |> Path.join(file <> ".html")
+    |> File.read
+    |> handle_file(conv)
+  end
+
+  def route(%{ path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} here!"}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{ conv | status: 403, resp_body: "Bears must never be deleted!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: #{byte_size(conv.resp_body)}
+    Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
   end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 
 end
 
-
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Emperor.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Emperor.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Emperor.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Emperor.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Emperor.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /pages/about HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
