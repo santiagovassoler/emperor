7c0a0a2f380dd8ad17d5204dc9699a36c09917d6
santiago vassoler <santiagovassoler@192.168.1.103>
2018-02-27 13:39:07 +0000

    changed port

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Emperor.MixProject do
   def project do
     [
       app: :emperor,
-      http: [port: {:system, "PORT"}]
+      http: [port: {:system, "PORT"}],
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
